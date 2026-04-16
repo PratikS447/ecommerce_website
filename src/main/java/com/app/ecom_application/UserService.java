41f783cd86e211b049205b8468dfe5a53a285d89
PratikS447 <shindepratik447@gmail.com>
2026-04-16 17:51:14 +0530

    Initialize commit

---
+++ src/main/java/com/app/ecom_application/UserService.java
@@ -0,0 +1,39 @@
+package com.app.ecom_application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+//    private List<User> userList = new ArrayList<>();
+//    private Long nextId = 1L;
+
+    public List<User> fetchAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public void addUser(User user){
+//        user.setId(nextId++);
+        userRepository.save(user);
+    }
+
+    public Optional<User> fetchUser(Long id){
+        return userRepository.findById(id);
+    }
+
+    public boolean updateUser(Long id, User updatedUser){
+        return userRepository.findById(id)
+                .map(existingUser -> {
+                    existingUser.setFirstName(updatedUser.getFirstName());
+                    existingUser.setLastName(updatedUser.getLastName());
+                    userRepository.save(existingUser);
+                    return true;
+                }).orElse(false);
+    }
+}
